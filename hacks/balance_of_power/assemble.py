e8e93baa58e4a9eb63d5dd27c67dcabd5f53fa83
Vincent Leclerc <thenpcnextdoor@pm.me>
2025-11-15 20:22:18 -0500

    Removed files

--- hacks/balance_of_power/assemble.py
+++
@@ -1,5 +0,0 @@
-from src.lib.structures.asm.script import Script
-
-if __name__ == "__main__":
-    script = Script.from_script_file(filename="asm/status_page.asm")
-    script.to_rom(output_path="test.smc", input_path="ff3.smc")
