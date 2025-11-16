c3799ff29d49704f7a0229b4eed040063017c61f
Vincent Leclerc <thenpcnextdoor@pm.me>
2025-11-15 22:34:17 -0500

    Fixed assembly file

---
+++ assemble.py
@@ -0,0 +1,27 @@
+import tomllib
+from pathlib import Path
+
+from src.lib.structures.asm.script import Script
+
+
+def assemble():
+    with open("config.toml") as f:
+        configs = tomllib.loads(f.read())["assembly"]
+
+    script_files = []
+
+    for filename in configs["scripts"]:
+        file = Path(filename)
+
+        if file.is_dir():
+            scripts = [f for f in file.iterdir() if f.is_file()]
+            script_files.append(*scripts)
+        else:
+            script_files.append(file)
+
+    script = Script.from_script_files(*script_files)
+    script.to_rom(input_path=configs["source"], output_path=configs["destination"])
+
+
+if __name__ == "__main__":
+    assemble()
